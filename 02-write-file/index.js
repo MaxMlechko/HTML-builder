7ddab3124369a932a1569726c42d3fc784815dd0
M4K5IMK4 <max_mlechko@mail.ru>
2022-05-22 15:19:13 +0300

    feat: 02 complete

---
+++ 02-write-file/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const path = require('path');
+const process = require('process');
+
+const rl = readline.createInterface({ input, output });
+const way = path.join('02-write-file', 'text.txt');
+
+fs.open(way, (err) => {
+  if(err) throw err;
+});
+fs.truncate(way , err => {
+  if(err) throw err;
+});
+
+const file = fs.createWriteStream(way);
+console.log('Hi, write some text:');
+rl.on('line', text => {
+
+  if(text == 'exit'){
+    rl.close();
+  }
+  else{
+    file.write(`${text}\n`);
+  }
+});
+
+process.on('exit', () => {
+  console.log('Ok, bye!');
+});
+
+process.on('SIGINT', () => {
+  console.log('Ok, bye!');
+});
